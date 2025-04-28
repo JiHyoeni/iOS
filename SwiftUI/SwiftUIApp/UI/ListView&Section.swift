aba76caebc390f7fd6d5788e7d8877142a23cf24
jihyeon <jihyoen0919@icloud.com>
2025-04-28 16:47:52 +0900

    - [SwiftUI] List EditButton

--- SwiftUI/SwiftUIApp/UI/ListView&Section.swift
+++ SwiftUI/SwiftUIApp/UI/ListView&Section.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct MyListView: View {
     var body: some View {
+        /*
         List {
             Section {
               HStack {
@@ -39,8 +40,23 @@ struct MyListView: View {
               Text("B group")
             }
         }
-
+*/
+        
+        NavigationView {
+            List {
+                ForEach(list, id: \.self) { item in
+                  Text(item)
+                }
+                .onDelete { list.remove(atOffsets: $0) }
+                .onMove { list.move(fromOffsets: $0, toOffset: $1)}
+            }
+            .navigationTitle("numbers")
+            .toolbar {
+                EditButton()
+            }
+        }
     }
+    @State var list = ["1", "2", "3", "4", "5"]
 }
 
 struct MyListView_Preview: PreviewProvider {
