b7737228efc69528e5400faa245bc8c18fa23156
jihyeon <jihyoen0919@icloud.com>
2025-04-29 08:31:54 +0900

    - Add App.swift

---
+++ SwiftUI/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by 박지현 on 2025/04/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
